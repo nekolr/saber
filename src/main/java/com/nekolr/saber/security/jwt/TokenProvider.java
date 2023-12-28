e47803402142f949fb335f6c5e5a59ffcb5a626f
nekolr <excalibll@163.com>
2023-12-28 17:41:54 +0800

    fix: token provider

--- src/main/java/com/nekolr/saber/security/jwt/TokenProvider.java
+++ src/main/java/com/nekolr/saber/security/jwt/TokenProvider.java
@@ -29,30 +29,20 @@ public class TokenProvider {
         Date expireDate = new Date(now + period.toMillis());
 
         return Jwts.builder()
-                .setSubject(username)
-                .signWith(secretKey, SignatureAlgorithm.HS256)
-                .setId(UUID.randomUUID().toString().replaceAll("-", ""))
-                .setExpiration(expireDate)
+                .subject(username)
+                .signWith(secretKey, Jwts.SIG.HS256)
+                .id(UUID.randomUUID().toString().replaceAll("-", ""))
+                .expiration(expireDate)
                 .compact();
-    }
-
-    public String getUsername(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(secretKey)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-
-        return claims.getSubject();
     }
 
     public Claims getClaims(String token) {
         try {
             return Jwts.parser()
-                    .setSigningKey(secretKey)
+                    .verifyWith(secretKey)
                     .build()
-                    .parseClaimsJws(token)
-                    .getBody();
+                    .parseSignedClaims(token)
+                    .getPayload();
         } catch (ExpiredJwtException e) {
             // token 过期
             log.error("Token has expired: {}", e.getMessage());
@@ -73,34 +63,5 @@ public class TokenProvider {
             log.error("Other exception: {}", e.getMessage());
         }
         return null;
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser()
-                    .setSigningKey(secretKey)
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (ExpiredJwtException e) {
-            // token 过期
-            log.error("Token has expired: {}", e.getMessage());
-        } catch (UnsupportedJwtException e) {
-            // token 格式错误
-            log.error("Token format error: {}", e.getMessage());
-        } catch (MalformedJwtException e) {
-            // token 构造错误
-            log.error("Token construct error: {}", e.getMessage());
-        } catch (SignatureException e) {
-            // 签名失败
-            log.error("Signature failed: {}", e.getMessage());
-        } catch (IllegalArgumentException e) {
-            // 非法参数
-            log.error("Illegal argument: {}", e.getMessage());
-        } catch (JwtException e) {
-            // 其他异常
-            log.error("Other exception: {}", e.getMessage());
-        }
-        return false;
     }
 }

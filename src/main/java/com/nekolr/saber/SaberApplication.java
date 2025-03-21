f1b9c2db29d39bb64ffca9c0c944f31507dc073c
nekolr <excalibll@163.com>
2025-03-21 21:54:49 +0800

    🚧 stable page json

--- src/main/java/com/nekolr/saber/SaberApplication.java
+++ src/main/java/com/nekolr/saber/SaberApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SaberApplication {
 
     public static void main(String[] args) {
